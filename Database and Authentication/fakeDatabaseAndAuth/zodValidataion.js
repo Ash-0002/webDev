d4064ad7ee0cc42e74c968788470dd3421dc6d31
ashish <rashish1313@gmail.com>
2024-04-09 00:18:56 +0530

    zod Validatoin

--- Database and Authentication/fakeDatabaseAndAuth/zodValidataion.js
+++ Database and Authentication/fakeDatabaseAndAuth/zodValidataion.js
@@ -1,20 +1,39 @@
 const express = require("express");
+const z = require("zod");
 
 const app = express();
+const schema = z.array(z.number());
 
 app.use(express.json());
 
+/* 
+{
+    email: String => email
+    password: altleast 8 letters  
+    country: "IN" | "US"
+} */
+
+const schema1 = z.object({
+    email: z.string(),
+    password: z.string(),
+    country: z.literal("IN").or(z.literal("US")),
+    kidneys: z.array(z.number())
+})
+
 app.post("/health-checkup", function(req, res) {
     const kidneys = req.body.kidneys;
-
-    res.send("You have" + kidneys.length + " kidneys");
+    const response = schema.safeParse(kidneys);
+    
+    if(!response.success) {
+        res.status(411).json({
+            msg: "Invalid inputs"
+        })
+    } else {
+        res.send({
+            response
+        })
+    }
 });
-
-app.use(function(err, req, res, next){
-    res.json({
-        msg: "Sorry something is up with our server"
-    })
-})
 
 
 //global catches
